5c8b1b8ce17e9b9c3c4038f09bf14d4a7f2cecb8
ferdsopadajr <ferdsopada@gmail.com>
2018-12-24 04:41:13 +0800

    Latest Commit

--- overallsystem/static/js/custom.js
+++ overallsystem/static/js/custom.js
@@ -1,5 +1,6 @@
 $(function() {
-	$(".sidebar-item li a").click(function() {
-		$(this).parent().addClass('active').siblings().removeClass('active');
-	});
+	$('.sidebar-nav li:not(:first-child) a').click(function() {
+		$(this).parent().addClass('active').siblings().removeClass('active').find('i').css('color','');
+		$(this).find('i').css('color','#2b5b84');
+	})
 });
